e5f87665f6125f76a2cfb61407c3c845bc5432fe
Tiago Curi <tiago.curi@voxeldigital.com.br>
2025-07-31 14:01:56 -0300

    feature: qsa stats data modeling implemented

--- qsa_stats.py
+++ qsa_stats.py
@@ -3,62 +3,76 @@
 from state import State
 from action import Action
 
+class QsaStats:
+        
+        def __init__(self, qsa : float):
+            print("Initializing QsaStats")
+            self.acc_qsa = qsa 
+            self.first_ocurrences_acc_qsa = qsa 
+            self.ocurrences_num = 1 # approach 1
+            self.first_ocurrences_num = 1 # approach 2
+        
+        def update_stats(self, qsa : float, first = False):
+            self.acc_qsa += qsa 
+            self.ocurrences_num += 1
+
+            if first:
+                self.first_ocurrences_acc_qsa += qsa 
+                self.first_ocurrences_num += 1
+
+        
+        def get_stats(self):
+            return (self.acc_qsa, self.ocurrences_num)
+        
+        
+        def get_first_ocurrences_stats(self):
+            return (self.acc_qsa, self.first_ocurrences_num)
+        
+        
+        def get_qsa_mean(self):
+            return self.acc_qsa / self.ocurrences_num
+        
+        
+        def get_qsa_first_ocurrences_mean(self):
+            return self.first_ocurrences_acc_qsa / self.first_ocurrences_num
+        
+        
+        # How to apply that only for first ocurrences?
+        def __add__(self, qsa : float):
+            self.acc_qsa += qsa
+            self.ocurrences_num += 1
+
+            return self
+        
+
 class QsaStatsManager:
 
-    class QsaStats:
-        def __init__(self):
-            print("Initializing QsaStats")
-
-            self.stats = (0., 0)
-        
-        def update_stats(self):
-            pass 
 
     def __init__(self):
-        self.__state_action_pairs_qsa : dict[tuple[State, Action], tuple[float, int]] = {} # tuple[float, int] -> sum of qsa's, 
-        # and number of ocurrences 
-
-    def get_state_action_qsa(self, state_action : tuple[State, Action]) -> float | None:
-        total_qsa_and_ocurrences_num = self.__state_action_pairs_qsa.get(state_action)
-        if total_qsa_and_ocurrences_num is not None:
-            total_qsa, ocurrences_num = total_qsa_and_ocurrences_num
-            return total_qsa / ocurrences_num
+        self.__qsa_stats : dict[tuple[State, Action], QsaStats]
         
-        return None 
-    
-    def __get_qsa_stats(self, state_action: tuple[State, Action]) -> tuple[float, int]:
-        qsa_stats = self.__state_action_pairs_qsa.get(state_action)
+    def __get_state_action_qsa_stats(self, state_action : tuple[State, Action]) -> QsaStats:
+        qsa_stats = self.__qsa_stats.get(state_action)
         if qsa_stats is not None:
             return qsa_stats
         
-        return (0.0, 0) 
+        return QsaStats(0.)
+         
 
-    def update_state_action_qsa(self, state_action : tuple[State, Action], qsa : float) -> bool:
-        try:
-            first_count : int = 1
-            #qsa_stats = { state_action: (qsa + acc_qsa, ocurrences_num + 1) for acc_qsa, ocurrences_num in self.__get_qsa_stats(state_action) if self.__state_action_pairs_qsa.__contains__(state_action) else state_action: (qsa, 1) }
-            qsa_stats = { state_action: self.__get_qsa_stats(state_action) }
-            self.__state_action_pairs_qsa.update(qsa_stats) 
-
-            self.__state_action_pairs_qsa[state_action] = (qsa, 1) 
-
-            return True 
-
-            total_qsa_and_ocurrences_num = self.__state_action_pairs_qsa.get(state_action)
-            if total_qsa_and_ocurrences_num is None:
-                self.__state_action_pairs_qsa[state_action] = (qsa, 1) 
-            if total_qsa_and_ocurrences_num is not None:
-                # car = {
-                #     "brand": "Ford",
-                #     "model": "Mustang",
-                #     "year": 1964
-                # }
-
-                # car.update({"color": "White"})
-                total_qsa, ocurrences_num = total_qsa_and_ocurrences_num
-                self.__state_action_pairs_qsa.update({ state_action: (total_qsa + qsa, ocurrences_num + 1) })
-                #self.__state_action_pairs_qsa[state_action] = (total_qsa + qsa, ocurrences_num + 1)
-        except:
-            return False     
+    def update_state_action_qsa_stats(self, state_action : tuple[State, Action], qsa : float):
+        # Update example:
+        # car = {
+        #     "brand": "Ford",
+        #     "model": "Mustang",
+        #     "year": 1964
+        # }
+        # car.update({"color": "White"})
+        #  
+        qsa_stats = { state_action: self.__get_state_action_qsa_stats(state_action) + qsa }
+        self.__qsa_stats.update(qsa_stats) 
         
-        return True 
+        
+        
+        
+    
+    

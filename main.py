c7b66bc3f0fe25500fe4b15cf1a148b7964a9d67
Tiago Curi <tiago.curi@voxeldigital.com.br>
2025-08-01 12:06:01 -0300

    feature: new abstracted qsa stats integrated with flow control

--- main.py
+++ main.py
@@ -1,70 +1,93 @@
 import random 
 from constants import *
 from action import Action
-from state import State
-from state_action_values import run as get_state_action_pairs_average_values
+from qsa_stats import QsaStatsManager
+# from state import State
+# from state_action_values import run as get_state_action_pairs_average_values
 
 # observação é a unidade mínima de informação dentro de um episódio. é o conjunto de propriedades 
-OBSERVATIONS = [] 
-episodes = []
+_observations = [] # observations being made (it always add an observation being currently evaluated) 
+episodes = [] # all episodes (now with diff epsilon values)
 
 # Example: { ((x, y), (ax, ay)): 233.64 } 
-def pick_action(
-        state_action_values : dict[tuple[tuple[int, int], tuple[int, int]], float], 
-        curr_state : tuple[int, int], epsilon : float) -> Action: 
+qsa_stats_manager = QsaStatsManager()
+def pick_action(curr_state : tuple[int, int], epsilon : float) -> Action: 
 
-    # ACTIONS:
-    # [(0, 1), (1, 0), (0, -1), (-1, 0)]
     # ACTIONS = [(0,1), (1, 0), (0, -1), (-1, 0)]  # up, right, down, left
     chance = random.random()
     action = None
     if (chance <= epsilon):
         action = random.choice(ACTIONS) 
     else:
-        population_average_qsa : dict[tuple[tuple[int, int], tuple[int, int]], float] = { }
-        for state_action_pair in [(curr_state, a) for a in ACTIONS]: 
-            average_qsa = state_action_values.get(state_action_pair) 
-            population_average_qsa[state_action_pair] = average_qsa if average_qsa is not None else 0
+        qsa_mean_population = []
+        for state_action_pair in [(State(curr_state[0], curr_state[1]), Action(a[0], a[1])) for a in ACTIONS]:
+            qsa_stats = qsa_stats_manager.__get_state_action_qsa_stats(state_action_pair) 
+            
+            # That is a qsa stats for the specific state action pair being iterated. It only considers
+            # the ocurrences from past experiences. experiences from the current episode are inserted
+            # at the end of it inside the method 'resume_episode' 
+            qsa_mean_population.append(qsa_stats.get_qsa_mean())
 
-        average_qsa_arr = list(population_average_qsa.values())
-        min_point = min(average_qsa_arr)
-        max_point = max(average_qsa_arr)
-        sample_space = max_point - min_point
-        if sample_space == 0:
+        print(f"Qsa mean Population: {qsa_mean_population}")
+
+        min_point = min(qsa_mean_population)
+        max_point = max(qsa_mean_population)
+        energy_landscape_sample = max_point - min_point
+        if energy_landscape_sample == 0:
             action_tuple = random.choice(ACTIONS)
             return Action(action_tuple[0], action_tuple[1])
 
-        #potentials = [sample_space / (value if value > 0 else 1) for value in average_qsa_arr] 
-        population_less_effort_potentials = { key: (sample_space / (value if value > 0 else 1)) for key, value in population_average_qsa.items() } # dictionaty comprehension
-        # weight goes up as effort goes down
-        potentials_sum_up = sum(list(population_less_effort_potentials.values()))
-        weights = { key: (value / potentials_sum_up) for key, value in population_less_effort_potentials.items() }
-    
+        # Divide the sample space for each of the qsa's means
+        energy_potentials = [energy_landscape_sample / (mean if mean > 0 else 1) for mean in qsa_mean_population]
+        total_energy = sum(energy_potentials)
+        # weight goes up as effort goes down (bigger weights for bigger energy potential values)
+        weights = [w / total_energy for w in energy_potentials]
         print(f"weights: {weights}")
-        # action = random.choices(list(weights.keys()), list(weights.values()))[0] ??
-        action = random.choices(ACTIONS, list(weights.values()))[0]
+
+        # The actions must be in order with weights (how come, review it)
+        action = random.choices(ACTIONS, weights)[0]
 
     if action is None:
         raise Exception
     
     return Action(action[0], action[1])
     
-def resume_episode(episode_number, epsilon):
+
+def resume_episode(episode_number, epsilon, terminal=False):
     import csv
 
     with open(f'./episodes/episode{episode_number}_{epsilon}.csv', 'w', newline='') as episodefile:
         observation_writer = csv.writer(episodefile, delimiter=',')
         observation_writer.writerow(["current_state", "action", "reward", "q(s, a)", "next_state", "additional_observation"])
         
-        # current_state, action, reward, qsa, next_state, monte_carlo_reward, observation 
-        for i in range(len(OBSERVATIONS)):
-            _observations = OBSERVATIONS[i:] 
-            qsa = calculate_qsa(_observations, 1) 
+        for i in range(len(_observations)):
+            observations_ = _observations[i:] 
+            qsa = calculate_qsa(observations_, 1) 
+
+            # current_state [0], action [1], reward [2], q(s, a), next_state [3], additional_observation [4]
+            curr_state = _observations[i][0]
+            action = _observations[i][1]
             observation_writer.writerow(
-                [str(OBSERVATIONS[i][0]), str(OBSERVATIONS[i][1]), str(OBSERVATIONS[i][2]), str(qsa), str(OBSERVATIONS[i][3]), str(OBSERVATIONS[i][4])])
+                [
+                    str(curr_state), 
+                    str(action), 
+                    str(_observations[i][2]), 
+                    str(qsa), 
+                    str(_observations[i][3]), 
+                    str(_observations[i][4])
+                ])
+            
+            qsa_stats_manager.update_state_action_qsa_stats(
+                (
+                    State(curr_state[0], curr_state[1]), 
+                    Action(action[0], action[1])
+                ), qsa)
+            
+            if terminal:
+                print("terminal report")
     
-    episodes.append(OBSERVATIONS.copy())
-    OBSERVATIONS.clear()
+    episodes.append(_observations.copy())
+    _observations.clear()
 
 
 def calculate_qsa(observations, curr_gama) -> int:
@@ -75,49 +98,62 @@ def calculate_qsa(observations, curr_gama) -> int:
     progressive_gama = curr_gama * GAMA
     return (gt * curr_gama) + calculate_qsa(observations[1:], progressive_gama)
 
-# Example: { ((x, y), (ax, ay)): 233.64 } 
-state_action_values = get_state_action_pairs_average_values()
 
 curr_state = INITIAL_STATE
 overall_episodes_counter = 1
 episode_size_counter = 1
-while overall_episodes_counter <= TRAINING_EPISODES_TOTAL_QUANTITY:
-    epsilon = INITIAL_EPSILON - (INITIAL_EPSILON * (overall_episodes_counter / TRAINING_EPISODES_TOTAL_QUANTITY))
-    #print(f"epsilon: {epsilon}")
+terminal_state = False
+is_training = overall_episodes_counter <= TRAINING_EPISODES_TOTAL_QUANTITY and not terminal_state
+
+while is_training:
     
-    action = pick_action(state_action_values, (curr_state.x, curr_state.y), epsilon) 
+    epsilon = INITIAL_EPSILON - (INITIAL_EPSILON * (overall_episodes_counter / TRAINING_EPISODES_TOTAL_QUANTITY))
+    end_training = not is_training
+    
+    action = pick_action((curr_state.x, curr_state.y), epsilon) 
+    
     # print(f"Moving {action.get_graphics()}") 
     # print(f"Now i am in the State: {curr_state}")
 
     next_candidate_state = curr_state + action
-    is_valid_move = not WALLS.__contains__(next_candidate_state) and ( (0 <= next_candidate_state.x <= WIDTH -1) and (0 <= next_candidate_state.y <= HEIGHT -1) )
+    is_valid_move = ( not WALLS.__contains__(next_candidate_state) and 
+                        ( (0 <= next_candidate_state.x <= WIDTH -1) and (0 <= next_candidate_state.y <= HEIGHT -1) ) )
+    
     is_forbidden_move = not is_valid_move
     if is_valid_move:
         next_state = next_candidate_state
+
+        # 3 base cases (casos de parada)
+        # 1. Terminal state
+        # 2. Training is over (has reached the number of episodes desired)
+        # training state verification
+        #  
         if next_state == TERMINAL_STATE: # do it should be explictly revealed? 
-            OBSERVATIONS.append(
+            _observations.append(
                 (curr_state, action, +10, next_state, "Successfully reached the maze’s exit."))
             
+            terminal_state = True 
+            continue 
+        elif episode_size_counter == EPISODE_SIZE:
             resume_episode(overall_episodes_counter, epsilon)
-
-            episode_size_counter = 0
+            episode_size_counter = 1
             overall_episodes_counter += 1
             curr_state = INITIAL_STATE
-            continue
         
-        OBSERVATIONS.append(
+        # It is not a terminal state
+        _observations.append(
             (curr_state, action, -1, next_state, "Move accepted"))
         curr_state = next_state
 
     elif is_forbidden_move:
-        OBSERVATIONS.append(
+        _observations.append(
             (curr_state, action, -10, curr_state, "Attempted to move into a wall or beyond the maze’s boundaries"))
-        
-    if episode_size_counter == EPISODE_SIZE:
-        resume_episode(overall_episodes_counter, epsilon)
-        episode_size_counter = 1
-        overall_episodes_counter += 1
-        curr_state = INITIAL_STATE
+
+    
+    
     else:
         episode_size_counter += 1
+
+    end_training = not (overall_episodes_counter <= TRAINING_EPISODES_TOTAL_QUANTITY)    
+    resume_episode(overall_episodes_counter, epsilon, terminal=(terminal_state or end_training))
 
--- qsa_stats.py
+++ qsa_stats.py
@@ -8,9 +8,16 @@ class QsaStats:
         def __init__(self, qsa : float):
             print("Initializing QsaStats")
             self.acc_qsa = qsa 
-            self.first_ocurrences_acc_qsa = qsa 
             self.ocurrences_num = 1 # approach 1
-            self.first_ocurrences_num = 1 # approach 2
+            # self.first_ocurrences_num = 1 - approach 2
+            # self.first_ocurrences_acc_qsa = qsa 
+
+        # How to apply that only for first ocurrences?
+        def __add__(self, qsa : float):
+            self.acc_qsa += qsa
+            self.ocurrences_num += 1
+
+            return self
         
         def update_stats(self, qsa : float, first = False):
             self.acc_qsa += qsa 
@@ -24,27 +31,19 @@ class QsaStats:
         def get_stats(self):
             return (self.acc_qsa, self.ocurrences_num)
         
-        
-        def get_first_ocurrences_stats(self):
-            return (self.acc_qsa, self.first_ocurrences_num)
-        
-        
+
         def get_qsa_mean(self):
             return self.acc_qsa / self.ocurrences_num
-        
-        
-        def get_qsa_first_ocurrences_mean(self):
-            return self.first_ocurrences_acc_qsa / self.first_ocurrences_num
-        
-        
-        # How to apply that only for first ocurrences?
-        def __add__(self, qsa : float):
-            self.acc_qsa += qsa
-            self.ocurrences_num += 1
 
-            return self
-        
 
+        # def get_first_ocurrences_stats(self):
+        #     return (self.acc_qsa, self.first_ocurrences_num)
+        
+        
+        # def get_qsa_first_ocurrences_mean(self):
+        #     return self.first_ocurrences_acc_qsa / self.first_ocurrences_num
+        
+    
 class QsaStatsManager:
 
 
@@ -60,14 +59,6 @@ class QsaStatsManager:
          
 
     def update_state_action_qsa_stats(self, state_action : tuple[State, Action], qsa : float):
-        # Update example:
-        # car = {
-        #     "brand": "Ford",
-        #     "model": "Mustang",
-        #     "year": 1964
-        # }
-        # car.update({"color": "White"})
-        #  
         qsa_stats = { state_action: self.__get_state_action_qsa_stats(state_action) + qsa }
         self.__qsa_stats.update(qsa_stats) 
         
